9d89fef16e80f5e2837f58a57cb9646a5419b655
Mustafa Muhhamed <s-mustafa.muhhamed@zewailcity.edu.eg>
2023-12-09 23:53:39 +0200

    {TMP}

--- PQueue.h
+++ PQueue.h
@@ -4,5 +4,36 @@
 
 #ifndef PQUEUE_H
 #define PQUEUE_H
+#pragma once
+#include <queue>
+
+template <typename T>
+class MyPriorityQueue {
+public:
+    void enqueue(const T& value) {
+        data.push(value);
+    }
+
+    void dequeue() {
+        if (!data.empty()) {
+            data.pop();
+        }
+    }
+
+    T front() const {
+        return data.top();
+    }
+
+    bool isEmpty() const {
+        return data.empty();
+    }
+
+    size_t size() const {
+        return data.size();
+    }
+
+private:
+    std::priority_queue<T> data;
+};
 
 #endif //PQUEUE_H
